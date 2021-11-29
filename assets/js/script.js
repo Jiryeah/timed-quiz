460b62fba19d2c0b82b3cf6625f60806e5886048
Chidori <business.faissal@outlook.com>
2021-11-28 23:13:21 -0600

    questions array/objects

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// variable with an array filled with objects for the questions 
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        values: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        values: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        values: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        values: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        values: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+];
